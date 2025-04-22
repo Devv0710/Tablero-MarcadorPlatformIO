9b0a8728f49929008b1b12faf1a8848e5a9f2906
Devv0710 <71566104+Devv0710@users.noreply.github.com>
2025-04-22 09:38:17 -0300

    Update Display.h
    Eliminacion de parametro adicionales no utilizados

--- src/Display.h
+++ src/Display.h
@@ -8,8 +8,8 @@ static uint8_t minutos_anteriores = 0, segundos_anteriores = 0;
 // Funciones para controlar displays
 void mostrarNumero(uint8_t num, uint8_t digitPosition, CRGB* display);
 void mostrarTiempo(uint8_t minutos, uint8_t segundos, CRGB* display);
-void mostrarPuntaje(uint16_t puntaje, CRGB* display, uint16_t numLeds);
-void mostrarContador(uint8_t valor, CRGB* display, uint16_t numLeds);
+void mostrarPuntaje(uint16_t puntaje, CRGB* display);
+void mostrarContador(uint8_t valor, CRGB* display);
 void actualizarDisplay();
 
 #endif  // DISPLAY_H
